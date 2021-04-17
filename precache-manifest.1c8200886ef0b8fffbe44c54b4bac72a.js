4f440d362feba638776348c3fa1f41946d193033
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2021-04-17 12:32:30 +0700

    initial commit

---
+++ precache-manifest.1c8200886ef0b8fffbe44c54b4bac72a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dce5169f2b6009d90e21",
+    "url": "/static/css/main.24a13298.chunk.css"
+  },
+  {
+    "revision": "dce5169f2b6009d90e21",
+    "url": "/static/js/main.dce5169f.chunk.js"
+  },
+  {
+    "revision": "dcb4138fdc682d52f680",
+    "url": "/static/js/1.dcb4138f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6668b3fdcca92521ee9c43b5367afb2f",
+    "url": "/index.html"
+  }
+];
